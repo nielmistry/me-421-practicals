ad561fc45498e9ed938839386cb652b306b7945e
Niel Mistry <niel@nielmistry.com>
2025-03-03 19:31:28 +0100

    first question

--- CE1/script.m
+++ CE1/script.m
@@ -1,6 +1,10 @@
 Ts = 0.25;
 stop_time = 50;
+mkdir("plots/")
 
+%% Unit Step Response
+
+fig = figure();
 simin = struct();
 simin.time = (0:Ts:50)';
 simin.signals.values = 0.7*(simin.time > 1);
@@ -8,3 +12,18 @@ simin.signals.values = 0.7*(simin.time > 1);
 result = sim("model.slx");
 
 plot(result.simout)
+title("Step Response")
+ylabel("Value")
+saveas(fig, "plots/ce1_step.png")
+
+
+%% Impulse Response
+fig = figure()
+simin.signals.values = 0.7*(simin.time == 1);
+result = sim("model.slx");
+plot(result.simout)
+
+title("Impulse Response")
+ylabel("Value")
+
+saveas(fig, "plots/ce1_impulse.png")
